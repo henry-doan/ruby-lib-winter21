bc7ebd9215d4db25d2113f25b5f42b76a4515638
Henry Doan <henrydoan96@gmail.com>
2021-11-10 21:56:08 -0700

    started

---
+++ library.rb
@@ -0,0 +1,30 @@
+# sudo code comment out thoughts, and break up into small 
+# manageable parts
+
+# store all of the books 
+  # hash, array, variable
+# display all of the books 
+  # puts, print or p
+  #  need to do multiple times 
+# add a book into the library
+  # insert, unshift, << in a array 
+  # get user info for the book 
+  # gets, assign a variable that a book has
+# create a  menu to navigate in the program
+# puts
+# 1, view all books 
+# 2, add a book 
+# 3, exit
+# gets, conditional
+# case when, if else 
+# methods to  organize 
+books = [
+  {title: 'Call to the wild', author: 'Bear girlz', year: 1990, pages: 600},
+  {title: 'cat in the hat', author: 'dr seuss', year: 1930, pages: 60}
+]
+
+def view_menu
+  puts "1. view all books"
+end
+
+view_menu
